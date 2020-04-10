831a8f5716452ddfb188a0e1246fd3aa5c26bf56
max <maximepouzol@gmail.com>
2020-04-10 11:51:26 +0200

    correction three

--- philo_three/thread.c
+++ philo_three/thread.c
@@ -70,12 +70,11 @@ int		*ft_processing(t_philo *philo, t_info *info)
 		pid[i] = fork();
 		if (pid[i] == 0)
 		{
-			philo[i].begin = ft_get_time();
 			philo[i].rest_bf_die = ft_get_time();
 			pthread_create(&philo[i].thread, NULL, ft_live, &philo[i]);
 			ft_dead(&philo[i], info);
 		}
-		usleep(50);
+		usleep(500);
 		i++;
 	}
 	ft_time_eat_af(philo, info);
@@ -105,6 +104,7 @@ int		ft_process(t_info *info, t_philo *philo)
 		ft_set_sem(&philo[i], se, sp, e);
 		philo[i].sem_eat = eat;
 		philo[i].number = i;
+		philo[i].begin = ft_get_time();
 	}
 	ft_processing(philo, info);
 	ft_close_sem(sp, se, e, eat);
